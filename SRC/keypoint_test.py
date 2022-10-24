164389b880a26ada82009012cd0cb58214002857
coding-geek1711 <mahestpm@gmail.com>
2022-10-24 22:13:30 +0530

    First Code commit

---
+++ SRC/keypoint_test.py
@@ -0,0 +1,30 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+from tqdm import tqdm
+import shutil
+
+# Video file
+VIDEO_SRC = 'video.mp4'
+
+# Read video src
+if os.path.exists('images'):
+    shutil.rmtree('images')
+
+if not os.path.exists('images'):
+    os.mkdir('images')
+
+cap = cv2.VideoCapture(VIDEO_SRC)
+
+
+frame_count = 0
+while cap.isOpened():
+    ret, frame = cap.read()
+
+    if ret:
+        cv2.imwrite(f'images/{frame_count}.jpg', frame)
+        frame_count += 1
+        cv2.imshow('frame', frame)
+cap.release()
+cv2.destroyAllWindows()
